7e3c6cdb1c814721a5eaa2bcd44986d71838e283
Bill Lau <656860993@qq.com>
2025-03-20 18:35:06 +0800

    init project from start.spring.io

---
+++ src/main/java/com/yitianyigexiangfa/unsplash_mcp_server/UnsplashMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.yitianyigexiangfa.unsplash_mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsplashMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsplashMcpServerApplication.class, args);
+	}
+
+}
